6966b559d2cd325efe6f745847418e1bf3302a6e
Tiago Guirado <tiago.guirado@justeattakeaway.com>
2021-05-18 21:16:13 +0200

    First commit

---
+++ TicketManagement.Domain/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿using TicketManagement.Domain.Common;
+using System;
+using System.Collections.Generic;
+
+namespace TicketManagement.Domain.Entities
+{
+    public class Category: AuditableEntity
+    {
+        public Guid CategoryId { get; set; }
+        public string Name { get; set; }
+        public ICollection<Event> Events { get; set; }
+    }
+}
